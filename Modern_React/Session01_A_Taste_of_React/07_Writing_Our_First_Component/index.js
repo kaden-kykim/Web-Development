8b0e81b4b1b20c4ff28665e2a8237aeb1dd9ecf7
kaden-kykim <kaden.kykim@gmail.com>
2020-07-13 19:46:29 -0700

    [React] Add Hello function component in comments

--- Modern_React/Session01_A_Taste_of_React/07_Writing_Our_First_Component/index.js
+++ Modern_React/Session01_A_Taste_of_React/07_Writing_Our_First_Component/index.js
@@ -10,4 +10,14 @@ class Hello extends React.Component {
 	}
 }
 
+// function Hello() {
+// 	return (
+// 		<div>
+// 			<h1>Hello there!</h1>
+// 			<h1>Hello there!</h1>
+// 			<h1>Hello there!</h1>
+// 		</div>
+// 	);
+// }
+
 ReactDOM.render(<Hello />, document.getElementById('root'));
